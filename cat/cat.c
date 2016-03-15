aaf98050843a6866d4b926d7834e8e43928403d9
Denis <profick123@gmail.com>
2016-03-15 04:54:59 +0300

    second hw

---
+++ cat/cat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUF_SIZE 256
+
+int main()
+{
+    char buffer[BUF_SIZE];
+    ssize_t retIn, retOut;
+
+    while ((retIn = read(STDIN_FILENO, &buffer, BUF_SIZE)) > 0) {
+        retOut = write(STDOUT_FILENO, &buffer, (ssize_t) retIn);
+        if (retOut < retIn) {
+            write(STDOUT_FILENO, &buffer + retIn - retOut, retIn - retOut);
+        }
+    }
+
+    return 0;
+}
