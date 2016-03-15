18b59fad030b518bef9add8af9f69275b3d9f42e
Denis <profick123@gmail.com>
2016-03-15 13:57:01 +0300

    modified

--- cat/cat.c
+++ cat/cat.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #define BUF_SIZE 256
 
-int main()
+int main(int argc, char *argv[])
 {
     char buffer[BUF_SIZE];
     ssize_t retIn, retOut;
+    int fd;
+    fd = open (argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("wrong input");
+        return -1;
+    }
+    while ((retIn = read(fd, &buffer, BUF_SIZE)) > 0 || retIn == -1) {
+        if (retIn == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                perror("error in reading");
+                return -1;
+            }
+        }
+        retOut = 0;
+        ssize_t tmp = 0;
+        while (retOut < retIn) {
+            tmp = write(STDOUT_FILENO, &buffer + retOut, retIn - retOut);
+            if (tmp == -1) {
+                if (errno == EINTR) {
+                    continue;
+                } else {
+                    perror("error in writing");
+                    return -1;
+                }
 
-    while ((retIn = read(STDIN_FILENO, &buffer, BUF_SIZE)) > 0) {
-        retOut = write(STDOUT_FILENO, &buffer, (ssize_t) retIn);
-        if (retOut < retIn) {
-            write(STDOUT_FILENO, &buffer + retIn - retOut, retIn - retOut);
+            }
+            retOut = retOut + tmp;
         }
     }
-
     return 0;
 }
